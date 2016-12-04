84b9ef6ec69b898c115f4d5a03a083fe8c871755
Mahdi AHMED <contact@mahdiahmed.be>
2016-12-04 19:02:11 +0100

    Ajout de l'exercice sur la suite de Fibonacci

---
+++ Fibonacci/Suite/SuiteFibo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suite
+{
+    public class SuiteFibo
+    {
+        public List<long>Suite(int number)
+        {
+            if (number < 0)
+            {
+                throw new FibonacciException(number, "Nombre négatif, impossible de continuer");
+            }
+            if (number < 2)
+            {
+                throw new FibonacciException(number, "Impossible de produire une suite ayant si peu de nombres");
+            }
+            List<long> suite = new List<long>();
+            suite.Capacity = number;
+            suite.Add(0);
+            suite.Add(1);
+            for (int position = 2;  position < number; position++)
+            {
+                suite.Add(suite[position - 1] + suite[position - 2]);
+            }
+            return suite;
+        }
+        public double GoldNumber(int number)
+        {
+            if (number<3)
+            {
+                throw new FibonacciException(number, "Il est impossible de calculer un nombre d'Or avec si peu d'élements");
+            }
+            List<long> fibo = new List<long>();
+            fibo = Suite(number);
+            double goldNumber = (fibo[number - 1]*1.0) / (fibo[number - 2]*1.0);
+            return goldNumber;
+        } 
+    }
+}
---
+++ Fibonacci/WUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Suite;
+
+namespace WUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Combien de nombres voulez-vous dans la suite de Fibonacci?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                SuiteFibo sFibo = new SuiteFibo();
+                List<long> fibo = sFibo.Suite(number);
+                StringBuilder sbFibo = new StringBuilder();
+                foreach (var item in fibo)
+                {
+                    sbFibo.Append("" + item);
+                }
+                Console.WriteLine("La Suite de Fibonacci est la suivante : " + sbFibo.ToString());
+                double goldNumber = sFibo.GoldNumber(number);
+                Console.WriteLine("Le nombre d'Or est évalué à  " + goldNumber.ToString());
+            }
+            catch (FibonacciException fibEx)
+            {
+                Console.WriteLine(fibEx.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur inconnue");
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
